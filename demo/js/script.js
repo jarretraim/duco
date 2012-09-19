c830697aa323fbb48646867a6ef93bc8981acea7
Jarret Raim <jarito@gmail.com>
2012-09-18 19:49:17 -0500

    datastore

--- demo/js/script.js
+++ demo/js/script.js
@@ -154,7 +154,56 @@ g.Shapes.ComplexProcess = graphiti.shape.basic.Circle.extend({
       this.label.setText(t);
     }
   }
+});
 
+g.Shapes.DataStore = graphiti.SetFigure.extend({
+  NAME : "graphiti.shape.icon.Icon",
+
+  init: function(width, height) {    
+    this._super();
+    
+    this.setDimension(100, 85);
+    this.setColor("#339BB9");
+    this.setBackgroundColor("#DDF4FB");
+    this.setCssClass("data_store");
+
+    // Label
+    this.label = new graphiti.shape.basic.WrappingLabel("New Process");
+    this.label.setFontColor("#339BB9");
+    this.label.setStroke(0);
+    this.addFigure(this.label, new graphiti.layout.locator.CenterLocator(this));
+
+    this.createPort("hybrid", new graphiti.layout.locator.TopLocator(this));
+    this.createPort("hybrid", new graphiti.layout.locator.RightLocator(this));
+    this.createPort("hybrid", new graphiti.layout.locator.LeftLocator(this));
+    this.createPort("hybrid", new graphiti.layout.locator.BottomLocator(this));
+
+    this.label.installEditor(new graphiti.ui.LabelEditor(this.label));
+  },
+
+  repaint : function(attributes)
+  {
+    if(this.repaintBlocked===true || this.shape===null){
+      return;
+    }
+
+    if (typeof attributes === "undefined") {
+      attributes = {};
+    }
+    
+    // redirect the bgColor to the inner set and not to the outer container
+    //
+    attributes.fill="none";
+    if(this.svgNodes !== null) {
+      this.svgNodes.attr({fill: this.bgColor.getHashStyle(), stroke:"#339BB9"});
+    }
+    
+    this._super(attributes);
+  },
+
+  createSet : function() {
+    return this.canvas.paper.path("M15.499,23.438c-3.846,0-7.708-0.987-9.534-3.117c-0.054,0.236-0.09,0.48-0.09,0.737v3.877c0,3.435,4.988,4.998,9.625,4.998s9.625-1.563,9.625-4.998v-3.877c0-0.258-0.036-0.501-0.09-0.737C23.209,22.451,19.347,23.438,15.499,23.438zM15.499,15.943c-3.846,0-7.708-0.987-9.533-3.117c-0.054,0.236-0.091,0.479-0.091,0.736v3.877c0,3.435,4.988,4.998,9.625,4.998s9.625-1.563,9.625-4.998v-3.877c0-0.257-0.036-0.501-0.09-0.737C23.209,14.956,19.347,15.943,15.499,15.943zM15.5,1.066c-4.637,0-9.625,1.565-9.625,5.001v3.876c0,3.435,4.988,4.998,9.625,4.998s9.625-1.563,9.625-4.998V6.067C25.125,2.632,20.137,1.066,15.5,1.066zM15.5,9.066c-4.211,0-7.625-1.343-7.625-3c0-1.656,3.414-3,7.625-3s7.625,1.344,7.625,3C23.125,7.724,19.711,9.066,15.5,9.066z");
+  }
 });
 
 g.Shapes.Interactor = graphiti.shape.basic.Rectangle.extend({
