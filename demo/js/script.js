f3bb84eefb6c38b9f88162d17d558e689c81e101
Jarret Raim <jarito@gmail.com>
2013-01-29 20:11:27 -0600

    Datastore icon color fix
    Hack to fix the color of the datastore icons since the CSS settings keep getting whacked.

--- demo/js/script.js
+++ demo/js/script.js
@@ -201,13 +201,18 @@ g.Shapes.ComplexProcess = draw2d.shape.basic.Circle.extend({
   getPersistentAttributes: g.Shapes.Common.getPersistentAttributes
 });
 
-g.Shapes.DataStore = draw2d.SetFigure.extend({
+g.Shapes.DataStore = draw2d.shape.icon.Db.extend({
   NAME : "g.Shapes.DataStore",
 
   init: function(width, height) {    
     this._super();    
     this.setDimension(100, 85);
     this.setCssClass("data-store");
+
+    // This shouldn't be necessary, but setting the CSS class doesn't propagate
+    // and I can't figure out why.
+    this.setBackgroundColor("#DDF4FB");
+    this.setColor("#339BB9");
 
     // Label
     this.label = new draw2d.shape.basic.WrappingLabel("New Data Store");
@@ -225,11 +230,7 @@ g.Shapes.DataStore = draw2d.SetFigure.extend({
 
   onDoubleClick: g.Shapes.Common.onDoubleClick,
   setProperties: g.Shapes.Common.setProperties,
-  getPersistentAttributes: g.Shapes.Common.getPersistentAttributes,
-
-  createSet : function() {
-    return this.canvas.paper.path("M15.499,23.438c-3.846,0-7.708-0.987-9.534-3.117c-0.054,0.236-0.09,0.48-0.09,0.737v3.877c0,3.435,4.988,4.998,9.625,4.998s9.625-1.563,9.625-4.998v-3.877c0-0.258-0.036-0.501-0.09-0.737C23.209,22.451,19.347,23.438,15.499,23.438zM15.499,15.943c-3.846,0-7.708-0.987-9.533-3.117c-0.054,0.236-0.091,0.479-0.091,0.736v3.877c0,3.435,4.988,4.998,9.625,4.998s9.625-1.563,9.625-4.998v-3.877c0-0.257-0.036-0.501-0.09-0.737C23.209,14.956,19.347,15.943,15.499,15.943zM15.5,1.066c-4.637,0-9.625,1.565-9.625,5.001v3.876c0,3.435,4.988,4.998,9.625,4.998s9.625-1.563,9.625-4.998V6.067C25.125,2.632,20.137,1.066,15.5,1.066zM15.5,9.066c-4.211,0-7.625-1.343-7.625-3c0-1.656,3.414-3,7.625-3s7.625,1.344,7.625,3C23.125,7.724,19.711,9.066,15.5,9.066z");
-  },  
+  getPersistentAttributes: g.Shapes.Common.getPersistentAttributes
 });
 
 g.Shapes.Interactor = draw2d.shape.basic.Rectangle.extend({
@@ -252,7 +253,7 @@ g.Shapes.Interactor = draw2d.shape.basic.Rectangle.extend({
 
   getShapeName: function() {
     return "interactor";
-  },
+  },    
 
   onDoubleClick: g.Shapes.Common.onDoubleClick,
   setProperties: g.Shapes.Common.setProperties,
